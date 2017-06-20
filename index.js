d9e9b49ae146f8a37949310018790f277e68e117
Josh Curtiss <git@curtiss.me>
2017-06-20 17:36:52 -0500

    Commit initial version of the code.

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ *  Patch the jQuery Validation Plugin to be friendly with Bootstrap's form feedback
+ *  By Josh Curtiss, 6/13/2016
+ *
+ *  You have a couple options how to utilize this patch:
+ *
+ *  1. Take the options object and call $.validator.setDefaults(jQueryValidationBootstrapOptions) yourself in $(document).ready() or whatever.
+ *  2. For your own convenience, you can call jQueryValidationBootstrapPatch() in your $(document).ready() or whatever you choose.
+ *  3. You could pick and choose the options out of this object and call them in your own $().validate() call if you want.
+ *
+ *  FOR THE IMPATIENT, DO THIS:  In your page load initialization script, call jQueryValidationBootstrapPatch() and you're done. 
+ *
+ */
+ 
+var jQueryValidationBootstrapOptions={
+	highlight: function(element) {
+		$(element)
+			.closest('.form-group')
+				.removeClass('has-success')
+				.addClass('has-error')
+				.end()
+			.closest('.input-group')
+				.find('.form-control-feedback')
+					.removeClass('glyphicon-ok')
+					.addClass('glyphicon-remove');
+	},
+	unhighlight: function(element) {
+		$(element)
+			.closest('.form-group')
+				.removeClass('has-error')
+				.addClass('has-success')
+				.end()
+			.closest('.input-group')
+				.find('.form-control-feedback')
+					.removeClass('glyphicon-remove')
+					.addClass('glyphicon-ok');
+	},
+	errorElement: 'span',
+	errorClass: 'help-block',
+	errorPlacement: function(error, element) {
+		if(element.parent('.input-group').length) {
+			error.insertAfter(element.parent());
+		} else {
+			error.insertAfter(element);
+		}
+	}
+};
+ 
+function jQueryValidationBootstrapPatch(){$.validator.setDefaults(jQueryValidationBootstrapOptions)}

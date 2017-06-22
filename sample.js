8b6e29e0558c6f1d24450de48290fc4b0e87083d
Josh Curtiss <J.Curtiss@RockValleyCollege.edu>
2017-06-22 09:52:32 -0500

    Added initial demo files.

---
+++ sample.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    // Apply the patch:
+    jQueryValidationBootstrapPatch();
+    // Do some validation:
+    $("#testform")
+        .validate({
+            debug: true,
+            rules: {
+                name: {required:true, minlength:3},
+                email: {required:true, email:true},
+                gender: {required:true},
+                usertype: {required:true}
+            },
+            invalidHandler: function(event,validator) {
+                $("#status").html("There are "+validator.numberOfInvalids()+" errors.");
+            },
+            submitHandler: function(form) {
+                $("#status").html("Passed validation!");
+            }
+        });
+});
